0732cd13af1e3df4acc5b4f4fc8bbe4e52ae6c4a
Alexander Bean Apmann <caboose200@gmail.com>
2016-12-03 15:41:50 -0500

    Added forms, user class

---
+++ CourseMan/Interface/AdminForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseMan.Interface
+{
+    public partial class AdminForm : Form
+    {
+        public AdminForm(String name)
+        {
+            
+
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- CourseMan/Interface/Form1.cs
+++ CourseMan/Interface/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CourseMan.Interface;
 
 namespace CourseMan
 {
@@ -15,6 +16,22 @@ namespace CourseMan
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "meme")
+            {
+                AdminForm form = new AdminForm("meme");
+                this.Hide();
+                form.Show();
+                
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
