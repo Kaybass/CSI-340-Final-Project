1d2322f5109ae64d0c77bb8369595becb8f259b8
Joe L <jlistro@gmail.com>
2016-12-03 17:45:14 -0500

    Added Singleton to handle course and section

---
+++ CourseMan/Domain/courseSectionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseMan.Domain
+{
+    class courseSectionHandler
+    {
+        private CourseID courseId;
+        private int sectionId;
+
+        private static courseSectionHandler instance = null;
+        
+        public courseSectionHandler() { }
+        
+        public static courseSectionHandler Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new courseSectionHandler();
+                }
+                return instance;
+            }
+        } 
+    }
+}
