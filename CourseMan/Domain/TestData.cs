1419143d9c0499fee2bc01c6bd3aaa95a62cbf3a
Joe L <jlistro@gmail.com>
2016-12-04 15:01:21 -0500

    Changed all user passwords to 'password' // Simplicity's sake

--- CourseMan/Domain/TestData.cs
+++ CourseMan/Domain/TestData.cs
@@ -18,14 +18,14 @@ namespace CourseMan.Domain
         {
             CourseSectionHandler csh = CourseSectionHandler.Instance;
 
-            User a = new User(1, "Adam.Acer", "pass0001", "Administrator", "CompSci");
-            User b = new User(2, "Brian.Banana", "pass0002", "Instructor", "CompSci");
-            User c = new User(3, "Chris.Cabana", "pass0003", "Student", "CompSci");
-            User d = new User(4, "Dell.Diesel", "pass0004", "Student", "CompSci");
-            User e = new User(5, "Evan.Edmund", "pass0005", "Student", "CompSci");
-            User f = new User(6, "Faith.Faker", "pass0006", "Student", "CompSci");
-            User g = new User(7, "Gabriella.Gilly", "pass0007", "Student", "CompSci");
-            User h = new User(8, "Harrison.Hammy", "pass0008", "Student", "CompSci");
+            User a = new User(1, "Adam.Acer", "password", "Administrator", "CompSci");
+            User b = new User(2, "Brian.Banana", "password", "Instructor", "CompSci");
+            User c = new User(3, "Chris.Cabana", "password", "Student", "CompSci");
+            User d = new User(4, "Dell.Diesel", "password", "Student", "CompSci");
+            User e = new User(5, "Evan.Edmund", "password", "Student", "CompSci");
+            User f = new User(6, "Faith.Faker", "password", "Student", "CompSci");
+            User g = new User(7, "Gabriella.Gilly", "password", "Student", "CompSci");
+            User h = new User(8, "Harrison.Hammy", "password", "Student", "CompSci");
 
             csh.AddUser(a);
             csh.AddUser(b);
