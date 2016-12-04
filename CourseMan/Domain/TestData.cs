11ae78908c07c9a997273b7fcd69225a74e85a43
Alexander Bean Apmann <caboose200@gmail.com>
2016-12-04 14:15:31 -0500

    Post lunch more work on UI

--- CourseMan/Domain/TestData.cs
+++ CourseMan/Domain/TestData.cs
@@ -18,14 +18,14 @@ namespace CourseMan.Domain
         {
             CourseSectionHandler csh = CourseSectionHandler.Instance;
 
-            User a = new User(0001, "Adam.Acer", "pass0001", "Administrator", "CompSci");
-            User b = new User(0002, "Brian.Banana", "pass0002", "Instructor", "CompSci");
-            User c = new User(0003, "Chris.Cabana", "pass0003", "Student", "CompSci");
-            User d = new User(0004, "Dell.Diesel", "pass0004", "Student", "CompSci");
-            User e = new User(0005, "Evan.Edmund", "pass0005", "Student", "CompSci");
-            User f = new User(0006, "Faith.Faker", "pass0006", "Student", "CompSci");
-            User g = new User(0007, "Gabriella.Gilly", "pass0007", "Student", "CompSci");
-            User h = new User(0008, "Harrison.Hammy", "pass0008", "Student", "CompSci");
+            User a = new User(1, "Adam.Acer", "pass0001", "Administrator", "CompSci");
+            User b = new User(2, "Brian.Banana", "pass0002", "Instructor", "CompSci");
+            User c = new User(3, "Chris.Cabana", "pass0003", "Student", "CompSci");
+            User d = new User(4, "Dell.Diesel", "pass0004", "Student", "CompSci");
+            User e = new User(5, "Evan.Edmund", "pass0005", "Student", "CompSci");
+            User f = new User(6, "Faith.Faker", "pass0006", "Student", "CompSci");
+            User g = new User(7, "Gabriella.Gilly", "pass0007", "Student", "CompSci");
+            User h = new User(8, "Harrison.Hammy", "pass0008", "Student", "CompSci");
 
             csh.AddUser(a);
             csh.AddUser(b);
@@ -43,7 +43,7 @@ namespace CourseMan.Domain
             List<MeetingTime> meetingTimes = new List<MeetingTime>();
             meetingTimes.Add(new MeetingTime(DayOfWeek.Tuesday, new TimeSpan(02, 00, 0), new TimeSpan(11, 15, 0)));
             meetingTimes.Add(new MeetingTime(DayOfWeek.Thursday, new TimeSpan(03, 00, 0), new TimeSpan(11, 15, 0)));
-            Section z = new Section(new SectionID("CSI", 340, 01), new Room("Wick", 101), meetingTimes);
+            Section z = new Section(new SectionID("CSI", 340, 01), new Room("Wick", 101), meetingTimes,2,20);
 
             csh.AddSection(z);
         }
--- CourseMan/Interface/Student.cs
+++ CourseMan/Interface/Student.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CourseMan.Domain;
+using CourseMan.Domain.ValueObjects;
+using CourseMan.Domain.Services;
 
 namespace CourseMan.Interface
 {
@@ -17,7 +20,45 @@ namespace CourseMan.Interface
 
         public void DoStudentThings()
         {
+            Console.Clear();
 
+            bool KeepGoing = true;
+
+            while (KeepGoing)
+            {
+                Console.WriteLine("Welcome to the Student console type:\nS to see your schedule\nC to see available Sections\nR to register for a section\nL to logout");
+                string Input = Console.ReadLine();
+
+                switch (Input)
+                {
+                    case "C":
+                    case "c":
+                        Console.Clear();
+                        foreach(KeyValuePair<SectionID, Section> p in CourseSectionHandler.Instance.Sections)
+                        {
+                            Console.WriteLine("SectionID: {0}\nCourseID: {1}\nCourse Name: {2}\nInstructor: {3}\nSeats: {4}/{5}\nFilled: {6}\nRoom: {7}",
+                                p.Value.SectionID,p.Value.CourseID,CourseSectionHandler.Instance.Courses[p.Value.CourseID],
+                                CourseSectionHandler.Instance.Users[p.Value.InstructorID].Username, p.Value.AvailableSeats,p.Value.MaxSeats,
+                                p.Value.IsFull, p.Value.Room);
+                        }
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case "S":
+                    case "s":
+                        break;
+                    case "R":
+                    case "r":
+                        break;
+                    case "L":
+                    case "l":
+                        KeepGoing = false;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input");
+                        break;
+                }
+            }
         }
     }
 }
